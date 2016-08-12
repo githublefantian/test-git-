e30171b447cd94cae2b889dcbc60f42a774d23ed
xiaoeasystack <mircosheng@gmail.com>
2016-08-12 08:38:23 -0400

    add GPL

--- readme-first.c
+++ readme-first.c
@@ -6,3 +6,5 @@ int main()
 //Git is free software.
 Git is a distributed version control system.
 Git is free software.
+//Git is a distributed version control system.
+//Git is free software distributed under the GPL.
